ea7f3dba1795fa67aca972117f7bd48c116eadd9
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-31 16:54:20 +0330

    start foreach and some changes in css

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const game=document.querySelector('.container'),
  cpu=document.querySelector('.cpu img'),
  resultText=document.querySelector('.reslut_text'),
  options=document.querySelectorAll('.option');
+
+ options.forEach(function(image,index){
+  image.addEventListener('click',(e)=>{
+    image.classList.add('active');
+
+    options.forEach(function(image2,index2){
+    if(index!==index2){
+      image2.classList.remove('active');
+    }
+    })
+  })
+ })
